872cfff0916a92d578e78c475e355ea2be400956
Devraj <devrajkanki@gmail.com>
2026-07-06 17:46:34 +0530

    Try explicit SMTP port for email

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -14,7 +14,9 @@ function getTransporter() {
     }
 
     transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: { user, pass },
     });
   }
